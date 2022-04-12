93a1f1c6b32e9f93bfbf3680a22826b2b61b9586
Maxivich <maxivlevich@gmail.com>
2022-04-12 15:45:36 +0300

    Add the 5 task

---
+++ src/main/java/ru/gb/Ivlev/homework1/Task5.java
@@ -0,0 +1,17 @@
+package ru.gb.Ivlev.homework1;
+
+public class Task5 {
+    public static void compareNumbers(){
+        int a = 10;
+        int b = 11;
+        if(a >= b){
+            System.out.println("a >= b");
+        }
+        else {
+            System.out.println("a < b");
+        }
+    }
+    public static void main(String[] args) {
+        compareNumbers();
+    }
+}
