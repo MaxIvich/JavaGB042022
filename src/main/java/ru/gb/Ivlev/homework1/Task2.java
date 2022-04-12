e6c910092075c68c80fdcac9d08dbe701d3ea165
Maxivich <maxivlevich@gmail.com>
2022-04-12 13:52:59 +0300

    Add the 2 task

---
+++ src/main/java/ru/gb/Ivlev/homework1/Task2.java
@@ -0,0 +1,16 @@
+package ru.gb.Ivlev.homework1;
+
+public class Task2 {
+    public static void printThreeWords() {
+        System.out.println("Banana");
+        System.out.println("Orange");
+        System.out.println("Apple");
+    }
+
+
+    public static void main(String[] args) {
+        printThreeWords();
+
+    }
+
+}
