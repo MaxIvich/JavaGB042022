f6482ed70a468709d9766e40505f089b25ff6e54
Maxivich <maxivlevich@gmail.com>
2022-04-18 19:39:09 +0300

    edits

--- src/main/java/ru/gb/Ivlev/homework2/task5.java
+++ src/main/java/ru/gb/Ivlev/homework2/task5.java
@@ -2,7 +2,7 @@ package ru.gb.Ivlev.homework2;
 
 public class task5 {
     private static boolean leapYear(int a) {
-        boolean leap = (a % 400 ==0||a % 4 == 0 && (!( a % 100 ==0) ));
+        boolean leap = (a % 400 ==0||a % 4 == 0 && a % 100 != 0);//(!( a % 100 ==0) ))
                 System.out.println(leap);
                 return leap;
       }
