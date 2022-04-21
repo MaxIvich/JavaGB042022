93bb1b63d06da8feb0b5239067fffb848f59bcef
Maxivich <maxivlevich@gmail.com>
2022-04-21 12:38:53 +0300

    change

--- src/main/java/ru/gb/Ivlev/homework3/task8.java
+++ src/main/java/ru/gb/Ivlev/homework3/task8.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class task8 {
     public static void methodShift(int[] arr,int n){
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < arr.length-n; i++) {
             for (int j = 0; j < arr.length-1; j++) {
                 int g = arr[j+1];
                 arr[j + 1] = arr[j];
@@ -16,7 +16,7 @@ public class task8 {
         System.out.println(a);
     }
     public static void main(String[] args) {
-        int[] arr = new int[]{1,2,3,4,5};
+        int[] arr = new int[]{1,2,3,4,5,6};
         methodShift(arr,2);
 
     }
