d83626e4496f4ab245840b3a58b8e9532e0b7394
Maxivich <maxivlevich@gmail.com>
2022-05-04 12:47:42 +0300

    homework5

--- src/main/java/ru/gb/Ivlev/homework5/Worker.java
+++ src/main/java/ru/gb/Ivlev/homework5/Worker.java
@@ -8,42 +8,54 @@ public class Worker {
     private int salary;
     private int age;
 
-    public String getFullname() {
+    public  String getFullname() {
         return fullname;
     }
+
     public void setFullname(String fullname) {
         this.fullname = fullname;
     }
+
     public String getPost() {
         return post;
     }
+
     public void setPost(String post) {
         this.post = post;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public int getPhoneNumber() {
         return phoneNumber;
     }
+
     public void setPhoneNumber(int phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
+
     public int getSalary() {
         return salary;
     }
+
     public void setSalary(int salary) {
         this.salary = salary;
     }
+
     public int getAge() {
         return age;
     }
+
     public void setAge(int age) {
         this.age = age;
     }
+
     public Worker(String fullname, String post, String email, int phoneNumber, int salary, int age){
             this.fullname = fullname;
             this.post = post;
@@ -53,39 +65,12 @@ public class Worker {
             this.age = age;
         }
 
-        public static  void printData(Worker a){
+         public String printData(Worker a) {
 
-        System.out.println(a.fullname +" "+ a.post+ " "+a.email+" "+a.phoneNumber+" "+a.salary+" "+a.age);
-    }
+                    String b = a.fullname + " " + a.post + " " + a.email + " " + a.phoneNumber + " " + a.salary + " " + a.age;
+                    System.out.println(b);
+                    return b;
 
-    public static void main(String[] args) {
-
-       Worker worker1 = new Worker("Иванов Иван Иванович", "engineer", "123@ru"
-               , 112358, 60000, 45);
-       Worker worker2 = new Worker("Петров Константин Андреевич", "engineer", "1234@ru"
-               , 112358, 20000, 19);
-       Worker worker3 = new Worker("Сидоров Александр Алексеевич", "engineer", "1235@ru"
-               , 112358, 50000, 35);
-       Worker worker4 = new Worker("Федоров Игорь Евгеньевич", "engineer", "1236@ru"
-               , 112358, 30000, 25);
-       Worker worker5 = new Worker("Лебедев Павел Викторович", "engineer", "1237@ru"
-               , 112358, 40000, 40);
-
-                    printData(worker1);
-
-           Worker[] workers = new Worker[5];
-           workers[0] =worker1;
-           workers[1] =worker2;
-           workers[2] =worker3;
-           workers[3] =worker4;
-           workers[4] =worker5;
-
-                for (int i = 0; i <workers.length ; i++) {
-                     if(workers[i].age>=40){
-                         System.out.println(workers[i].fullname +" "+ workers[i].post +" "+ workers[i].email+" "+workers[i].phoneNumber+" "+
-                             workers[i].salary+" "+workers[i].age);
-                 }
-        }
-    }
+         }
 }
 
